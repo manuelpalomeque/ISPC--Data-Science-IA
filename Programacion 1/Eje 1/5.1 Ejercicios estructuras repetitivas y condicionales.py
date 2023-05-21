c8a3c5e68ddf676ee38a34526cd58d33b252ada9
manuelpalomeque <pjonathanmanuel@hotmail.com>
2023-05-21 13:00:21 -0300

    Ejercicio 3 resuelto

--- Programacion 1/Eje 1/5.1 Ejercicios estructuras repetitivas y condicionales.py
+++ Programacion 1/Eje 1/5.1 Ejercicios estructuras repetitivas y condicionales.py
@@ -52,7 +52,32 @@ El numero minimo es: {}'''.format(cantMayores100, cantMenos100, max, min))
 
 # 3. Ingresar las edades y el sexo de 15 personas y determinar cuántas son mujeres, cuántos varones, cuántos mayores
 # de edad y cuántos menores de edad.
+xPersonas = 0
+mayoresEdad = 0
+menoresEdad = 0
+cantFem = 0
+cantMasc = 0
 
+while xPersonas < 14:
+    edad = int(input('Ingrese la edad: '))
+    sexo = input('Ingrese el genero, M para masculino y F para femenino: ')
+    if edad >= 18:
+        mayoresEdad += 1
+    else:
+        menoresEdad += 1
+    if sexo == 'F':
+        cantFem += 1
+    elif sexo == 'M':
+        cantMasc += 1
+    else:
+        print('debe ingresar F o M, intente nuevamente')
+    xPersonas += 1
+
+print('''
+La cantidad de mayores de edad es: {}
+La cantidad de menos de edad es: {} 
+La cantidad de Mujeres es: {}
+La cantidad de varones es: {}'''.format(mayoresEdad, menoresEdad, cantFem, cantMasc))
 
 # 4. Leer 10 números y mostrar solamente los números positivos, y su sumatoria.
 
