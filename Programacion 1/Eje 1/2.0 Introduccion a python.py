05e54a97f3eedda9a240451113ef0b2ef2a722a2
manuelpalomeque <pjonathanmanuel@hotmail.com>
2023-05-21 11:53:12 -0300

    Ejercicios de Introduccion

---
+++ Programacion 1/Eje 1/2.0 Introduccion a python.py
@@ -0,0 +1,32 @@
+# TIPOS DE DATOS:
+
+# Numericos:
+a = 5       # un entero
+b = 3.14    # un flotante
+c = 2+3j    # un nùmero complejo
+
+# Cadena de caracteres:
+nombre = 'Juan'
+apellido = "Perez"
+mensaje = 'Hola, ¿Como estas?'
+
+# Booleanos:
+d = True
+e = False
+
+# Operadores Aritmeticos:
+f = 5 + 3   # Suman
+g = 10 - 7  # Resta
+h = 2 * 4   # Multiplicacion
+i = 15 / 3  # Division
+j = 15 // 4 # Division entera
+k = 15 % 4  # Modulo
+l = 2 ** 3  # Potencia
+
+print(f, g, h, i, j, k, l)
+
+# Operaciones de Comparacion:
+ab = 5
+bc = 5
+print(ab == bc) # igualdad
+
