6ecbd8951dab7cfa9e5ea3c5a236edd58056287f
manuelpalomeque <pjonathanmanuel@hotmail.com>
2023-06-23 17:51:39 -0300

    Ejercicio 1

---
+++ Programacion 1/Eje 1/Ejercicio Programación estructurada y modular.py
@@ -0,0 +1,30 @@
+# Ejercicio Programación estructurada y modular:
+
+# 1. Realice un programa que muestre el mensaje “Hola Aula X (Indicar el número de aula a la que
+# pertenecen), ¿Qué tal?” en tres veces intercambiados entre ellos otro mensajes a su elección.
+
+def saludar():
+    print('Hola Aula 1, ¿Qué tal?')
+    r1 = input('Respuesta: ')
+    print('Me alegro, como te llamas?')
+    r2 = input('Respuesta: ')
+    print('{}!,un gusto conocerte!'.format(r2))
+
+saludar()
+
+# 2. A partir del siguiente ejemplo “Hola Ana, ¿Qué tal?” realizar el programa la ejecución del mismo con
+# al menos otros dos nombres más, es decir, tres mensajes con tres nombres distintos. Recuerda: en
+# estos ejercicios trabajamos argumentos.
+
+
+# 3. Realizar un programa de funciones que contengan 3 parámetros, el cual derive en una suma. Mostrar
+# el resultado dos veces.
+
+
+# 4. Realice un programa que lea dos números (dos parámetros), compare si son IGUALES, en ese caso,
+# mostrar un mensaje que muestre TRUE.
+
+
+# 5. Realice un programa que contenga una función que se llame “sumayresta”, que la misma contenga dos
+# variables locales nombradas suma y resta, respectivamente. Recuerda: en estos ejercicios trabajamos
+# argumentos para este ejercicio sería dos.
