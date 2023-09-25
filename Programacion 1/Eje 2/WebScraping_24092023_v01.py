9e2701e9ba0e6a002dbadb2eabd74b20908a37c5
manuelpalomeque <pjonathanmanuel@hotmail.com>
2023-09-24 21:00:30 -0300

    Practica de web scraping- Pagina web titanic

--- Programacion 1/Eje 2/WebScraping_24092023_v01.py
+++ Programacion 1/Eje 2/WebScraping_24092023_v01.py
@@ -7,7 +7,7 @@ import requests
 # parser: sirve para extraer la informacion del archivo html. Trabaja con Beautiful Soup
 import lxml
 
-# 2 Proceso ----------------------------------------------------------
+# 2 Extraer el codigo HTML de una pagina ----------------------------------------------------------
 # Extraere el titulo y el contenido
 
 # Definir en la variable "website", la web en la que voy a trabajar
@@ -26,4 +26,24 @@ soup = BeautifulSoup(contenido,'lxml')
 # Nos muestra el codigo html pero dificil de leer:
 # print(soup)
 # se recomienda usar .prettify para mejorar la visualizacion:
-print(soup.prettify())
+# print(soup.prettify())
+
+# 3 Obtener los datos que queremos --------------------------------------------------
+
+# extraer el titulo
+box= soup.find('article', class_='main-article')
+nombrePelicula = box.find('h1').get_text()
+print('El nombre de la pelicula es: {}'.format(nombrePelicula))
+
+# extraer el guion:
+nombreGuion = box.find('div', class_='full-script').get_text()
+#print('El guion es: {}'.format(nombreGuion))
+
+# devuelve el guion perno no esta bien visualmente, por lo cual agrego funciones:
+# strip: borra espacios vacios al inicio y al final
+#separator:  reemplaza saltos de linea con un espacio en blanco
+nombreGuion1 = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+print('El guion es: {}'.format(nombreGuion1))
+
+#4  Exportar lo extraido en un archivo de texto ---------------------------------------
+
